a3e2737276af7d9f933d544675a8b581c9dab62d
dohaoo0 <dohao15102004@gmail.com>
2024-04-13 22:58:02 +0700

    QLCB

---
+++ grant.sql
@@ -0,0 +1,30 @@
+﻿--1.	Phân quyền chọn cho người dùng có tên QuyenKH trên đối tượng là CHUYENBAY trên CSDL có tên QLCB
+CREATE LOGIN KH
+WITH PASSWORD = '123456';
+GO
+
+CREATE USER QuyenKH FOR LOGIN KH;
+GO
+
+USE QLCB 
+Grant SELECT ON CHUYENBAY TO QuyenKH;
+GO 
+
+--2.	Phân cho người dùng có tên Nhanvien : 
+--INSERT vào bảng CHUYENBAY, 
+--DELETE trong bảng KM, 
+--UPDATE trong bảng TUYENBAY 
+
+CREATE LOGIN NV
+WITH PASSWORD = 'Nhanvien';
+GO
+
+CREATE USER Nhanvien FOR LOGIN NV;
+GO
+
+GRANT INSERT ON CHUYENBAY TO Nhanvien;
+GRANT DELETE ON KM TO Nhanvien;
+GRANT UPDATE ON TUYENBAY TO Nhanvien;
+
+--3.Thu hồi quyền SELECT của người dùng có tên QuyenKH trên đối tượng là CHUYENBAY trên CSDL có tên QLCB
+REVOKE SELECT ON CHUYENBAY FROM QuyenKH;
---
+++ query.sql
@@ -0,0 +1,31 @@
+﻿USE QLCB
+-- Xây dựng thủ tục tra cứu thông tin chuyến bay: Số hiệu chuyến bay, giờ bay, giờ đến, giá vé
+CREATE PROCEDURE TraCuuChuyenBay
+    @SH_CB VARCHAR(20)
+AS
+BEGIN
+    SELECT SH_CB, GIO_BAY, GIO_DEN, GIA_VE
+    FROM CHUYENBAY
+    WHERE SH_CB = @SH_CB
+END;
+GO
+
+EXEC TraCuuChuyenBay 'CB2';
+
+--Cho biết số lượng các chuyến bay có lượng vé đã bán ra bằng 70% lượng vé tối đa
+SELECT COUNT(*) AS SLCB
+FROM THUCHIEN
+WHERE LG_VE_MAX = 0.7 * LG_VE_MAX;
+
+--Thống kê tổng số lượng vé đã bán ra của từng chuyến bay có khuyến mãi
+SELECT SH_CB, SUM(LG_VE_MAX)
+FROM THUCHIEN
+GROUP BY SH_CB
+HAVING SH_CB IN (SELECT SH_CB FROM KM_CB);
+
+-- Thống kê tổng số chuyến bay và tổng số vé đã bán ra của các chuyến bay có khuyến mãi
+COUNT SELECT(DISTINCT SH_CB), SUM(LG_VE_MAX)
+FROM THUCHIEN
+WHERE SH_CB IN (SELECT SH_CB FROM KM_CB);
+
+
